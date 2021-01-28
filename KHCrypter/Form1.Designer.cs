fc4bd37878d237d19fdef31df0fe389ee8d99a15
DrCrypterkh <78143075+DrCrypterkh@users.noreply.github.com>
2021-01-28 16:40:45 +0700

    Add files via upload

---
+++ KHCrypter/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.materialFlatButton4 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.materialFlatButton5 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.pictureIcon = new System.Windows.Forms.PictureBox();
+            this.btnChooseIcon = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtIcon = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(500, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 35);
+            this.textBox1.TabIndex = 0;
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Icon = null;
+            this.materialFlatButton1.Location = new System.Drawing.Point(324, 80);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(145, 36);
+            this.materialFlatButton1.TabIndex = 1;
+            this.materialFlatButton1.Text = "Load Target *EXE";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(407, 174);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 201);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Icon = null;
+            this.materialFlatButton2.Location = new System.Drawing.Point(410, 132);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(117, 36);
+            this.materialFlatButton2.TabIndex = 3;
+            this.materialFlatButton2.Text = "ENCODE64 EXE";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(151, 97);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(98, 19);
+            this.materialLabel1.TabIndex = 6;
+            this.materialLabel1.Text = "URL Injection";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 150);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(303, 35);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // materialFlatButton4
+            // 
+            this.materialFlatButton4.AutoSize = true;
+            this.materialFlatButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton4.Depth = 0;
+            this.materialFlatButton4.Icon = null;
+            this.materialFlatButton4.Location = new System.Drawing.Point(132, 456);
+            this.materialFlatButton4.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton4.Name = "materialFlatButton4";
+            this.materialFlatButton4.Primary = false;
+            this.materialFlatButton4.Size = new System.Drawing.Size(108, 36);
+            this.materialFlatButton4.TabIndex = 10;
+            this.materialFlatButton4.Text = "Crypter EXE";
+            this.materialFlatButton4.UseVisualStyleBackColor = true;
+            this.materialFlatButton4.Click += new System.EventHandler(this.MaterialFlatButton4_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(127, 401);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(138, 29);
+            this.linkLabel2.TabIndex = 13;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "AV Scanner";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(77, 210);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(232, 29);
+            this.linkLabel1.TabIndex = 14;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "HOST URL Malware";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Flag_of_Cambodia;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 210);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // materialFlatButton5
+            // 
+            this.materialFlatButton5.AutoSize = true;
+            this.materialFlatButton5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton5.Depth = 0;
+            this.materialFlatButton5.Icon = null;
+            this.materialFlatButton5.Location = new System.Drawing.Point(608, 132);
+            this.materialFlatButton5.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton5.Name = "materialFlatButton5";
+            this.materialFlatButton5.Primary = false;
+            this.materialFlatButton5.Size = new System.Drawing.Size(97, 36);
+            this.materialFlatButton5.TabIndex = 15;
+            this.materialFlatButton5.Text = "Binary EXE";
+            this.materialFlatButton5.UseVisualStyleBackColor = true;
+            this.materialFlatButton5.Click += new System.EventHandler(this.materialFlatButton5_Click_1);
+            // 
+            // pictureIcon
+            // 
+            this.pictureIcon.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureIcon.Location = new System.Drawing.Point(606, 391);
+            this.pictureIcon.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureIcon.Name = "pictureIcon";
+            this.pictureIcon.Size = new System.Drawing.Size(109, 61);
+            this.pictureIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureIcon.TabIndex = 19;
+            this.pictureIcon.TabStop = false;
+            // 
+            // btnChooseIcon
+            // 
+            this.btnChooseIcon.Location = new System.Drawing.Point(671, 456);
+            this.btnChooseIcon.Margin = new System.Windows.Forms.Padding(2);
+            this.btnChooseIcon.Name = "btnChooseIcon";
+            this.btnChooseIcon.Size = new System.Drawing.Size(44, 35);
+            this.btnChooseIcon.TabIndex = 16;
+            this.btnChooseIcon.Text = "...";
+            this.btnChooseIcon.UseVisualStyleBackColor = true;
+            this.btnChooseIcon.Click += new System.EventHandler(this.btnChooseIcon_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(440, 410);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 29);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Icon Path";
+            // 
+            // txtIcon
+            // 
+            this.txtIcon.Location = new System.Drawing.Point(407, 456);
+            this.txtIcon.Margin = new System.Windows.Forms.Padding(2);
+            this.txtIcon.Name = "txtIcon";
+            this.txtIcon.Size = new System.Drawing.Size(244, 35);
+            this.txtIcon.TabIndex = 17;
+            this.txtIcon.TextChanged += new System.EventHandler(this.txtIcon_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 504);
+            this.Controls.Add(this.pictureIcon);
+            this.Controls.Add(this.btnChooseIcon);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtIcon);
+            this.Controls.Add(this.materialFlatButton5);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.materialFlatButton4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.Text = "KHCrypter Advance Crypter Toolkit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton5;
+        private System.Windows.Forms.PictureBox pictureIcon;
+        private System.Windows.Forms.Button btnChooseIcon;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.TextBox txtIcon;
+    }
+}
+
---
+++ KHCrypter/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+using System.Reflection;
+using System.Resources;
+using WindowsFormsApp1.Resources;
+using CrypterExample;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MaterialForm
+    {
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Green800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            {
+                var OpenFileDialog = new OpenFileDialog();
+                OpenFileDialog.Title = "Load File EXE";
+                OpenFileDialog.ShowDialog();
+                textBox1.Text = OpenFileDialog.FileName;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                Interaction.MsgBox("Please select a file", MsgBoxStyle.Critical);
+            }
+            else
+            {
+                richTextBox1.Text = Convert.ToBase64String(File.ReadAllBytes(textBox1.Text));
+            }
+        }
+
+    
+        // make it like this 
+        // where?
+      
+
+        private void materialFlatButton1_Click_1(object sender, EventArgs e)
+        {
+            {
+                var OpenFileDialog = new OpenFileDialog();
+                OpenFileDialog.Title = "Load File EXE";
+                OpenFileDialog.ShowDialog();
+                textBox1.Text = OpenFileDialog.FileName;
+            }
+        }
+
+        private void materialFlatButton3_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == null)
+            {
+                Interaction.MsgBox("Please paste some base64 characters", MsgBoxStyle.Critical);
+            }
+            else
+            {
+                var SaveFile = new SaveFileDialog();
+                SaveFile.Filter = "Exe Files (*.exe) | *.exe";
+                if (SaveFile.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.WriteAllBytes(SaveFile.FileName, Convert.FromBase64String(richTextBox1.Text));
+                        Interaction.MsgBox("Done", MsgBoxStyle.Information);
+                    }
+                    catch (Exception)
+                    {
+                        Interaction.MsgBox("Invalid base64 characters", MsgBoxStyle.Exclamation);
+                    }
+                }
+            }
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://antiscan.me");
+        }
+
+        private void linkLabel1_LinkClicked_1(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://ghostbin.co");
+        }
+
+        private void materialFlatButton5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaterialFlatButton4_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            ///  make load resource string1 + textBox2
+
+            string code = Resource1.String1;
+
+            code = code.Replace("textBox2", textBox2.Text);
+
+            ///  than compile to exe with codedomcompiler
+            
+            
+
+
+            var SaveFile = new SaveFileDialog();
+            SaveFile.Filter = "Exe Files (*.exe) | *.exe";
+            if (SaveFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+
+                    // ok, but time started now no just pay in 11:10 nevermind :) ok :)
+                    // test code
+                    // should add icon button make it work 
+                    // because u see that save output - icon selected from open file dialog
+
+                    //good but can you make it  add icon than save exe 
+
+
+                    // test?
+                    if (txtIcon.Text != string.Empty)
+                    {
+                        Compiler.CompileFromSource(code, SaveFile.FileName,  txtIcon.Text);
+                    }
+                    else
+                    {
+                        Compiler.CompileFromSource(code, SaveFile.FileName);
+                    }
+                    
+
+
+                    Interaction.MsgBox("Done", MsgBoxStyle.Information);
+                }
+                catch (Exception)
+                {
+                    Interaction.MsgBox("Compile failed", MsgBoxStyle.Exclamation);
+                }
+            }
+            
+        }
+
+        private void materialFlatButton5_Click_1(object sender, EventArgs e)
+        {
+            // like this - ok
+            // this is how you call this method load from exe 
+
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                Interaction.MsgBox("Please select a file", MsgBoxStyle.Critical);
+            }
+            else
+            {
+                richTextBox1.Text = Encrypt(File.ReadAllBytes(textBox1.Text));
+            }
+            // done? more please wait
+        }
+
+        public string Encrypt(byte[] V)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            int num1 = checked(V.Length - 1);
+            int index = 0;
+            while (index <= num1)
+            {
+                byte num2 = V[index];
+                stringBuilder.Append(Convert.ToString(num2, 2).PadLeft(8, '0'));
+                stringBuilder.Append(" ");
+                checked { ++index; }
+            }
+            return stringBuilder.ToString().Substring(0, checked(stringBuilder.ToString().Length - 1));
+        }
+
+        private void materialFlatButton6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChooseIcon_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Icon (*.ico)|*.ico";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtIcon.Text = openFileDialog.FileName;
+                    pictureIcon.ImageLocation = openFileDialog.FileName;
+                    pictureIcon.BorderStyle = BorderStyle.FixedSingle;
+                }
+                else
+                {
+                    txtIcon.Text = string.Empty;
+                    pictureIcon.ImageLocation = string.Empty;
+                }
+            }
+        }
+
+        private void txtIcon_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
